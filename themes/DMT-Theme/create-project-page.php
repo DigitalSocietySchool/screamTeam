774063a6042879457bbeaeba6798e40ee2391964
Abdelrahman Hassan <egyabdel@gmail.com>
2016-12-15 13:57:15 +0100

    submit form using REST
    -header includes nesccary wpi script
    - added script to submit JSON obj to peojects REST endpoint

--- themes/DMT-Theme/create-project-page.php
+++ themes/DMT-Theme/create-project-page.php
@@ -1,14 +1,23 @@
 <?php
 
-	global $wp_session;
-	$wp_session['searching'] = false;
+global $wp_session;
+$wp_session['searching'] = false;
 /*
  Template Name: Custom Create Project Page
  *
 */
-?>
+add_action("wp_enqueue_scripts", "enqueue_");
+function enqueue_() {
+    wp_enqueue_script( 'wp-api', plugins_url( 'build/js/wp-api.js', __FILE__ ), array( 'jquery', 'underscore', 'backbone' ), '1.0', true );
+    $settings = array( 'root' => esc_url_raw( get_rest_url() ), 'nonce' => wp_create_nonce( 'wp_rest' ) );
+    wp_localize_script( 'wp-api', 'WP_API_Settings', $settings );
+    wp_register_script('wp-api');
 
-<?php get_header(); ?>
+}
+
+
+ get_header(); ?>
+
 
     <div id= "main-content">
         <div id = "sidebar_box">
@@ -53,5 +62,42 @@
         <button type="button" id = "createteam_btn" class = "scream_btn">Create a team</button>
     </div>
         </div>
+
+ <script type="text/javascript">
+
+
+var jsonObj={
+    "title": { "raw": "Alien Team9"}, 
+    "type": "project",
+    "slug": "alien-team-2",
+      "status": "publish",
+                
+    "wpcf-project-goal": "to make a good website",
+    "wpcf-start-date" : "1480550400",
+    "wpcf-end-date": "1483056000"
+}
+    $(function() {
+        $('#createteam_btn').click(function(){
+            console.log("clicked");
+      $.ajax({
+
+            type: "POST",
+           url:   "http://localhost/wp-json/wp/v2/project",  //to be changed into dynamic url relative to directory
+           dataType:'jsonp',
+           data: jsonObj,
+           beforeSend: function ( xhr ) {
+        xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
+
+       },
+       success: function(data){console.log(data) },
+       error: function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log(XMLHttpRequest);
+            console.log(textStatus);
+            console.log(errorThrown);}
+    
+});
+   });
+  });
+    </script> 
 
 <?php get_footer(); ?>
