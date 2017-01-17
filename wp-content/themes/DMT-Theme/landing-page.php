954e8857a2897c3af9f4e4347319c1f4b5948a88
Abdelrahman Hassan <egyabdel@gmail.com>
2017-01-17 12:03:00 +0100

    update files

---
+++ wp-content/themes/DMT-Theme/landing-page.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ Template Name: Landing-Pgage
+ *
+*/
+
+
+get_header(); ?>
+
+    <div id="bigcontainer" style="background-color: #F4F4F4;width:100%;height:100%;" >
+
+        <div id="innerContainer1" style="height:50%;width:100;display:flex;flex-direction:row;margin-left: 3em;/* width: 50%; */">
+                <div id="innerinner" style="height:50%;width:100;display:flex;flex-direction:column;">
+                     <div class="boldTitle" >Set your goals, choose your methods, work your magic</div>
+                      <div class="subheader" style="padding-left: 20%;font-size: 65%;">Design Method Toolkit for Agile,team-based project</div>
+                       <div class="next_btn" style="width: 60%;height: 60%;box-shadow: black;margin-left: 20%;margin-top: 2%;line-height:300%;font-size:90%">EXPLORE DESIGN METHOD TOOLKIT</div>
+         </div>
+                  <img src="/wp-content/themes/DMT-Theme/library/images/landing1" style="width: 50%s;">
+
+         </div>
+
+       
+
+
+        <div id="innerContainer2" style="height:50%;width:100;display:flex;flex-direction:row;">
+            
+        </div>
+          
+   
+    </div>
+
+<?php get_footer(); ?>
